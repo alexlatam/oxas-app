7973690a78b128d6b4026268fa0d9c681923b9b5
Alexis Montilla <alexisamm9261@gmail.com>
2022-10-20 18:03:00 -0400

    refactor: add mysql user

--- administracion/common/conexion.php
+++ administracion/common/conexion.php
@@ -1,5 +1,4 @@
 <?php
 //$conn = new mysqli("ip_host","user","pass_user","dbname");
-// $conn = new mysqli("localhost","root","","oxa");
 $conn = new mysqli("localhost","oxas","oxas_meli_app","oxa");
 if ($conn->connect_error){die("Connection failed: ".$conn->connect_error);} ?>
