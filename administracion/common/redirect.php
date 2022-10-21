3666fd6938eff4eb09bbd92ca2456d381f165011
Alexis Montilla <alexisamm9261@gmail.com>
2022-10-20 21:51:16 -0400

    refactor: test index.php

--- administracion/common/redirect.php
+++ administracion/common/redirect.php
@@ -2,7 +2,7 @@
 session_start();
 require 'meli.php';
 include 'conexion.php';
-include '../Oxa/Funciones.php';
+include 'funciones.php';
 
 date_default_timezone_set('America/Caracas');
 $meli = new Meli($_SESSION['client_id'], $_SESSION['secret_key']);
