914187758e668e2d069407ac8438ee17a4e71d95
Alexis Montilla <alexisamm9261@gmail.com>
2022-10-20 21:20:58 -0400

    refactor: test index.php

--- registro.php
+++ registro.php
@@ -18,10 +18,10 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo $row['field'];
     echo "<br>";
-    if    ($row['field'] == 'https_url_app'){ $_SESSION["https_url_app"] = $row['https_url_app'];}
-    elseif($row['field'] == 'secret_key'){    $_SESSION["secret_key"]    = $row['secret_key'];}
-    elseif($row['field'] == 'redirect_url'){  $_SESSION["redirect_url"]  = $row['redirect_url'];}
-    elseif($row['field'] == 'client_id'){     $_SESSION["client_id"]     = $row['client_id'];}
+    if    ($row['field'] == 'https_url_app'){ $_SESSION["https_url_app"] = $row['value'];}
+    elseif($row['field'] == 'secret_key'){    $_SESSION["secret_key"]    = $row['value'];}
+    elseif($row['field'] == 'redirect_url'){  $_SESSION["redirect_url"]  = $row['value'];}
+    elseif($row['field'] == 'client_id'){     $_SESSION["client_id"]     = $row['value'];}
     
   }
 }
