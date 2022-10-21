405e55fa04931749f533601b63b510f32eb6e985
Alexis Montilla <alexisamm9261@gmail.com>
2022-10-20 22:55:48 -0400

    refactor: index.php

--- administracion/common/navbar.php
+++ administracion/common/navbar.php
@@ -31,79 +31,57 @@ $total_preguntas = $consulta->total;
   </button>
   <div class='collapse navbar-collapse' id='navbarResponsive'>
     <ul class='navbar-nav navbar-sidenav border-right border-muted' id='exampleAccordion'>
-      <!-- <?php if (isset($_COOKIE['_validate'])) {
-        if ($_COOKIE['_validate'] != 1) {
-          $sqlOX = "SELECT IDREVENDEDOR FROM revendedores WHERE USUARIOID=$id_user LIMIT 1";
-          if ($conn->query($sqlOX)) {
-            $resultOX = $conn->query($sqlOX);
-            if ($resultOX->num_rows > 0) {
-              while ($rowOX = $resultOX->fetch_assoc()) {
-      ?>
-                <li class='nav-item ' data-toggle='tooltip' data-placement='right' title='OxaShop'>
-                  <a class='nav-link ' href='/administracion/oxashop/'>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='19px' viewBox='0 0 640 512'>
-                      <path fill="#bc6dcc" d='M128 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm-24-80h192v48h48v-48h192v48h48v-57.59c0-21.17-17.23-38.41-38.41-38.41H344v-64h40c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32H256c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h40v64H94.41C73.23 224 56 241.23 56 262.41V320h48v-48zm264 80h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm240 0h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32z' />
-                    </svg>
-                    <span class='nav-link-text'>OxaShop</span>
-                  </a>
-                </li>
-          <?php
-              }
-            }
-          }
-          ?> -->
-          <li class='nav-item ' data-toggle='tooltip' data-placement='right' title='Respuestas Automáticas'>
-            <a class='nav-link ' href='/administracion/respuestas_automaticas/'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='19px' viewBox='0 0 640 512'>
-                <path fill="#bc6dcc" d='M128 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm-24-80h192v48h48v-48h192v48h48v-57.59c0-21.17-17.23-38.41-38.41-38.41H344v-64h40c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32H256c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h40v64H94.41C73.23 224 56 241.23 56 262.41V320h48v-48zm264 80h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm240 0h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32z' />
-              </svg>
-              <span class='nav-link-text'>Respuestas Automáticas</span>
-            </a>
-          <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Preguntas Pendientes'>
-            <a class='nav-link' href='/administracion/preguntas.php'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 576 512'>
-                <path fill="#bc6dcc" d='M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z' />
-              </svg>
-              <span class='nav-link-text'>Preguntas</span>
-              <?php if ($total_preguntas != 0) { ?><span class="align-self-center badge_preg"><?php echo $total_preguntas; ?></span><?php } ?>
-            </a>
-          </li>
-          <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Publicaciones'>
-            <a class='nav-link' href='/administracion/publicaciones/'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 512 512'>
-                <path fill="#bc6dcc" d='M128 116V76c0-8.837 7.163-16 16-16h352c8.837 0 16 7.163 16 16v40c0 8.837-7.163 16-16 16H144c-8.837 0-16-7.163-16-16zm16 176h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zM16 144h64c8.837 0 16-7.163 16-16V64c0-8.837-7.163-16-16-16H16C7.163 48 0 55.163 0 64v64c0 8.837 7.163 16 16 16zm0 160h64c8.837 0 16-7.163 16-16v-64c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v64c0 8.837 7.163 16 16 16zm0 160h64c8.837 0 16-7.163 16-16v-64c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v64c0 8.837 7.163 16 16 16z' />
-              </svg>
-              <span class='nav-link-text icon_public'>Publicaciones</span>
-            </a>
-          </li>
 
-          <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Ventas'>
-            <a class='nav-link' href='/administracion/ventas/'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 576 512'>
-                <path fill="#bc6dcc" d='M504.717 320H211.572l6.545 32h268.418c15.401 0 26.816 14.301 23.403 29.319l-5.517 24.276C523.112 414.668 536 433.828 536 456c0 31.202-25.519 56.444-56.824 55.994-29.823-.429-54.35-24.631-55.155-54.447-.44-16.287 6.085-31.049 16.803-41.548H231.176C241.553 426.165 248 440.326 248 456c0 31.813-26.528 57.431-58.67 55.938-28.54-1.325-51.751-24.385-53.251-52.917-1.158-22.034 10.436-41.455 28.051-51.586L93.883 64H24C10.745 64 0 53.255 0 40V24C0 10.745 10.745 0 24 0h102.529c11.401 0 21.228 8.021 23.513 19.19L159.208 64H551.99c15.401 0 26.816 14.301 23.403 29.319l-47.273 208C525.637 312.246 515.923 320 504.717 320zM408 168h-48v-40c0-8.837-7.163-16-16-16h-16c-8.837 0-16 7.163-16 16v40h-48c-8.837 0-16 7.163-16 16v16c0 8.837 7.163 16 16 16h48v40c0 8.837 7.163 16 16 16h16c8.837 0 16-7.163 16-16v-40h48c8.837 0 16-7.163 16-16v-16c0-8.837-7.163-16-16-16z' />
-              </svg>
-              <span class='nav-link-text'>Ventas</span>
-              <?php if ($total_ventas_abiertas != 0) { ?><span class="align-self-center badge_vent"><?php echo $total_ventas_abiertas; ?></span><?php } ?>
-            </a>
-          </li>
-          <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Estadísticas'>
-            <a class='nav-link' href='/administracion/mensajeria/'>
-              <svg xmlns="http://www.w3.org/2000/svg" width='17px' viewBox="0 0 512 512">
-                <path fill="#bc6dcc" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" />
-              </svg>
-              <span class='nav-link-text icon_public'>Mensajeria PostVenta</span>
-            </a>
-          </li>
-          <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Estadísticas'>
-            <a class='nav-link' href='/administracion/estadisticas/'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 512 512'>
-                <path fill="#bc6dcc" d='M500 384c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H12c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v308h436zM372.7 159.5L288 216l-85.3-113.7c-5.1-6.8-15.5-6.3-19.9 1L96 248v104h384l-89.9-187.8c-3.2-6.5-11.4-8.7-17.4-4.7z' />
-              </svg>
-              <span class='nav-link-text icon_public'>Estadísticas</span>
-            </a>
-          </li>
-      <!-- <?php }
-      } ?> -->
+      <li class='nav-item ' data-toggle='tooltip' data-placement='right' title='Respuestas Automáticas'>
+        <a class='nav-link ' href='/administracion/respuestas_automaticas/'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='19px' viewBox='0 0 640 512'>
+            <path fill="#bc6dcc" d='M128 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm-24-80h192v48h48v-48h192v48h48v-57.59c0-21.17-17.23-38.41-38.41-38.41H344v-64h40c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32H256c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h40v64H94.41C73.23 224 56 241.23 56 262.41V320h48v-48zm264 80h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm240 0h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32z' />
+          </svg>
+          <span class='nav-link-text'>Respuestas Automáticas</span>
+        </a>
+      <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Preguntas Pendientes'>
+        <a class='nav-link' href='/administracion/preguntas.php'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 576 512'>
+            <path fill="#bc6dcc" d='M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z' />
+          </svg>
+          <span class='nav-link-text'>Preguntas</span>
+          <?php if ($total_preguntas != 0) { ?><span class="align-self-center badge_preg"><?php echo $total_preguntas; ?></span><?php } ?>
+        </a>
+      </li>
+      <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Publicaciones'>
+        <a class='nav-link' href='/administracion/publicaciones/'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 512 512'>
+            <path fill="#bc6dcc" d='M128 116V76c0-8.837 7.163-16 16-16h352c8.837 0 16 7.163 16 16v40c0 8.837-7.163 16-16 16H144c-8.837 0-16-7.163-16-16zm16 176h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zM16 144h64c8.837 0 16-7.163 16-16V64c0-8.837-7.163-16-16-16H16C7.163 48 0 55.163 0 64v64c0 8.837 7.163 16 16 16zm0 160h64c8.837 0 16-7.163 16-16v-64c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v64c0 8.837 7.163 16 16 16zm0 160h64c8.837 0 16-7.163 16-16v-64c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v64c0 8.837 7.163 16 16 16z' />
+          </svg>
+          <span class='nav-link-text icon_public'>Publicaciones</span>
+        </a>
+      </li>
+
+      <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Ventas'>
+        <a class='nav-link' href='/administracion/ventas/'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 576 512'>
+            <path fill="#bc6dcc" d='M504.717 320H211.572l6.545 32h268.418c15.401 0 26.816 14.301 23.403 29.319l-5.517 24.276C523.112 414.668 536 433.828 536 456c0 31.202-25.519 56.444-56.824 55.994-29.823-.429-54.35-24.631-55.155-54.447-.44-16.287 6.085-31.049 16.803-41.548H231.176C241.553 426.165 248 440.326 248 456c0 31.813-26.528 57.431-58.67 55.938-28.54-1.325-51.751-24.385-53.251-52.917-1.158-22.034 10.436-41.455 28.051-51.586L93.883 64H24C10.745 64 0 53.255 0 40V24C0 10.745 10.745 0 24 0h102.529c11.401 0 21.228 8.021 23.513 19.19L159.208 64H551.99c15.401 0 26.816 14.301 23.403 29.319l-47.273 208C525.637 312.246 515.923 320 504.717 320zM408 168h-48v-40c0-8.837-7.163-16-16-16h-16c-8.837 0-16 7.163-16 16v40h-48c-8.837 0-16 7.163-16 16v16c0 8.837 7.163 16 16 16h48v40c0 8.837 7.163 16 16 16h16c8.837 0 16-7.163 16-16v-40h48c8.837 0 16-7.163 16-16v-16c0-8.837-7.163-16-16-16z' />
+          </svg>
+          <span class='nav-link-text'>Ventas</span>
+          <?php if ($total_ventas_abiertas != 0) { ?><span class="align-self-center badge_vent"><?php echo $total_ventas_abiertas; ?></span><?php } ?>
+        </a>
+      </li>
+      <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Estadísticas'>
+        <a class='nav-link' href='/administracion/mensajeria/'>
+          <svg xmlns="http://www.w3.org/2000/svg" width='17px' viewBox="0 0 512 512">
+            <path fill="#bc6dcc" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" />
+          </svg>
+          <span class='nav-link-text icon_public'>Mensajeria PostVenta</span>
+        </a>
+      </li>
+      <li class='nav-item' data-toggle='tooltip' data-placement='right' title='Estadísticas'>
+        <a class='nav-link' href='/administracion/estadisticas/'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='17px' viewBox='0 0 512 512'>
+            <path fill="#bc6dcc" d='M500 384c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H12c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v308h436zM372.7 159.5L288 216l-85.3-113.7c-5.1-6.8-15.5-6.3-19.9 1L96 248v104h384l-89.9-187.8c-3.2-6.5-11.4-8.7-17.4-4.7z' />
+          </svg>
+          <span class='nav-link-text icon_public'>Estadísticas</span>
+        </a>
+      </li>
       <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Planes y Suscripción">
         <a class="nav-link" href="/administracion/suscripcion/">
           <svg xmlns="http://www.w3.org/2000/svg" width="17px" class="svg-primary" viewBox="0 0 576 512">
@@ -118,7 +96,6 @@ $total_preguntas = $consulta->total;
           </svg>
           <span class='nav-link-text'>Configuración General</span>
         </a>
-      </li>
       </li>
     </ul>
     <ul class='navbar-nav sidenav-toggler'>
