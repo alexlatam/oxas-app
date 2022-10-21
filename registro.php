18337fc2560cf83e7104e8c76e1745d3d5230973
Alexis Montilla <alexisamm9261@gmail.com>
2022-10-20 21:21:50 -0400

    refactor: test index.php

--- registro.php
+++ registro.php
@@ -4,9 +4,7 @@ session_start();
 require 'administracion/common/meli.php';
 require 'administracion/common/conexion.php';
 
-if ( !isset($_GET['site']) ) {
-  header("location: index.php");
-}
+if ( !isset($_GET['site']) ) { header("location: index.php"); }
 
 $siteId      = $_GET['site'];
 
@@ -14,10 +12,8 @@ $sql="SELECT * FROM settings;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  echo "AQUI<br>";
   while($row = $result->fetch_assoc()) {
-    echo $row['field'];
-    echo "<br>";
+
     if    ($row['field'] == 'https_url_app'){ $_SESSION["https_url_app"] = $row['value'];}
     elseif($row['field'] == 'secret_key'){    $_SESSION["secret_key"]    = $row['value'];}
     elseif($row['field'] == 'redirect_url'){  $_SESSION["redirect_url"]  = $row['value'];}
@@ -25,8 +21,6 @@ if ($result->num_rows > 0) {
     
   }
 }
-var_dump($_SESSION['secret_key']);
-var_dump(Meli::$AUTH_URL[$siteId]);
-die();
+
 $meli = new Meli($_SESSION['client_id'], $_SESSION['secret_key']);
 header("location: " . $meli->getAuthUrl( $_SESSION['redirect_url'], Meli::$AUTH_URL[$siteId] ));
